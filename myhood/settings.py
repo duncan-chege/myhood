476312a7abedba8f7ebf8760d118f0849d2a8b05
dunyung1 <dshege4@gmail.com>
2019-03-23 15:10:28 +0300

    add register and profile html files

--- myhood/settings.py
+++ myhood/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL ='home'
