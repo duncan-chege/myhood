1e2167933d625a7a8cf428f7ef697810022164e5
dunyung1 <dshege4@gmail.com>
2019-03-23 15:29:15 +0300

    add profile route, import login required class

--- block/urls.py
+++ block/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns=[
     url('^$',views.home, name = 'home'),
     url('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    url('register/', views.register, name ='register')
+    url('register/', views.register, name ='register'),
+    url('profile/', views.profile, name='profile')
 ]
--- block/views.py
+++ block/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def register(request):
+
+@
 def profile(request):
     return render(request, 'users/profile.html')
