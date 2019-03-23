ef9dc282db97680eebd7bb21d90eaf1cbf7ae6a9
dunyung1 <dshege4@gmail.com>
2019-03-23 19:29:00 +0300

    add post condition in register view & home routes

--- block/urls.py
+++ block/urls.py
@@ -3,8 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
-    url('^$',views.home, name = 'home'),
-    url('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    url('register/', views.register, name ='register'),
-    url('profile/', views.profile, name='profile')
+    url(r'^$',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    url(r'register/', views.register, name ='register'),
+    url(r'^home',views.home, name='home'),
+    url(r'profile/', views.profile, name='profile')
 ]
