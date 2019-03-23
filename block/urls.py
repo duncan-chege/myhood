9f43eb3b74ddec37e917bf431be9a9cb6526bbc7
dunyung1 <dshege4@gmail.com>
2019-03-23 14:09:02 +0300

    add a templates folder for storing html files

--- block/urls.py
+++ block/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
-    url('^$',views.home, name = 'home')
+    url('^$',views.home, name = 'home'),
+    url('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
+    url('register/', views.register, name ='register')
 ]
