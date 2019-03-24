c7e8d3cb30d2792a175eaa671afa66de124b5736
dunyung1 <dshege4@gmail.com>
2019-03-24 07:50:43 +0300

    create tests and add a new Model

--- block/models.py
+++ block/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Neighbourhood(models.Model):
+    hood_name= models.CharField(max_length =50)
+    hood_location= models.CharField(max_length =50)
+    occupants= models.IntegerField()
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):      #customize the way models are referenced in admin by adding this
+        return self.hood_name
+
+    
+
--- block/tests.py
+++ block/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+class NeighbourhoodTest(TestCase):
+    def test_string_representation(self):
+        hood1 = Neighbourhood(hood_name='Jiji')
+        self.assertEqual(str(hood1), hood1.hood_name)
+
+    def setUp(self):
+        self.new_hood = Neighbourhood(id=1, hood_name="Jiji",hood_location="Umoja",occupants=80)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_hood, Neighbourhood))
+    
+    
+
+   
